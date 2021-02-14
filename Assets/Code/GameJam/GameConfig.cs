f3c46afb7f3015a204e00ea260d5c3a264af6337
Colin Bellino <bellino.colin@gmail.com>
2021-02-14 16:32:38 +0100

    ✨ Add basic unit spawning and selection

--- Assets/Code/GameJam/GameConfig.cs
+++ Assets/Code/GameJam/GameConfig.cs
@@ -5,6 +5,6 @@ namespace GameJam
 	[CreateAssetMenu(menuName = "Game Jam/Game Config")]
 	public class GameConfig : ScriptableObject
 	{
-
+		public CharacterComponent UnitPrefab;
 	}
 }
--- Assets/Code/GameJam/GameState.cs
+++ Assets/Code/GameJam/GameState.cs
@@ -1,7 +1,14 @@
-﻿namespace GameJam
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam
 {
 	public class GameState
 	{
-		
+		public List<Character> AllCharacters;
+		public List<Character> SelectedCharacters;
+		public bool SelectionInProgress;
+		public Vector3 SelectionStart;
+		public Vector3 SelectionEnd;
 	}
 }
