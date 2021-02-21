0b2e798d5140d5cdbaf0432af0fdc64f71163fbc
Colin Bellino <bellino.colin@gmail.com>
2021-02-21 05:44:07 +0100

    ✨ Add ui animations

--- Assets/Code/GameJam/GameUI.cs
+++ Assets/Code/GameJam/GameUI.cs
@@ -1,10 +1,11 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using TMPro;
 using Unity.VectorGraphics;
 using UnityEngine;
 using UnityEngine.UI;
-using Button = UnityEngine.UI.Button;
 
 namespace GameJam
 {
@@ -15,21 +16,21 @@ namespace GameJam
 		[SerializeField] private SVGImage[] _countdownImages;
 		[SerializeField] private Text _debugText;
 		[Header("Victory")]
-		[SerializeField] private GameObject _victoryRoot;
+		[SerializeField] private Image _victoryRoot;
 		[SerializeField] private TMP_Text _victoryText;
 		[SerializeField] private TMP_Text _titleText;
 		[SerializeField] public Button VictoryNextButton;
-		[SerializeField] private GameObject _commentRoot;
+		[SerializeField] private Image _commentRoot;
 		[SerializeField] public Button CommentNextButton;
-		[SerializeField] private GameObject _retryRoot;
+		[SerializeField] private Image _retryRoot;
 		[SerializeField] public Button RetryYesButton;
 		[SerializeField] public Button RetryNoButton;
 		[Header("Defeat")]
-		[SerializeField] private GameObject _giveUpRoot;
+		[SerializeField] private Image _giveUpRoot;
 		[SerializeField] private TMP_Text _giveUpText;
 		[SerializeField] public Button GiveUpYesButton;
 		[SerializeField] public Button GiveUpNoButton;
-		[SerializeField] private GameObject _receiveHelpRoot;
+		[SerializeField] private Image _receiveHelpRoot;
 		[SerializeField] private TMP_Text _receiveHelpText;
 		[SerializeField] public Button ReceiveHelpYesButton;
 		[SerializeField] public Button ReceiveHelpNoButton;
@@ -39,21 +40,39 @@ namespace GameJam
 		private void Awake()
 		{
 			HideGameplay();
-			HideVictory();
-			HideGiveUp();
-			HideReceiveHelp();
-			HideRetry();
-			HideComment();
+			HideVictory(0f);
+			HideGiveUp(0f);
+			HideReceiveHelp(0f);
+			HideRetry(0f);
+			HideComment(0f);
 			HideTitle(0f);
 		}
 
 		public void ShowGameplay() { _gameplayRoot.SetActive(true); }
 		public void HideGameplay() { _gameplayRoot.SetActive(false); }
 
-		public async UniTask ShowTitle() => await _titleText.DOFade(1f, 0.5f);
-		public async UniTask HideTitle(float duration = 0.5f) => await _titleText.DOFade(0f, duration);
+		public void SetCounter(int deathCounter)
+		{
+			_countdownImages[0].gameObject.SetActive(deathCounter < 1);
+			_countdownImages[1].gameObject.SetActive(deathCounter < 2);
+			_countdownImages[2].gameObject.SetActive(deathCounter < 3);
+		}
 
-		public void ShowVictory(bool helpReceived)
+		public void SetDebugText(string value)
+		{
+			_debugText.text = value;
+		}
+
+		public async UniTask ShowTitle()
+		{
+			await _titleText.DOFade(1f, 0.5f);
+		}
+		public async UniTask HideTitle(float duration = 0.5f)
+		{
+			await _titleText.DOFade(0f, duration);
+		}
+
+		public async UniTask ShowVictory(bool helpReceived)
 		{
 			if (helpReceived)
 			{
@@ -63,36 +82,33 @@ namespace GameJam
 			{
 				_victoryText.text = $"You managed to beat the game all by yourself, impressive! But remember there is no shame in accepting the help of others from time to time.";
 			}
-			_victoryRoot.SetActive(true);
-		}
-		public void HideVictory() { _victoryRoot.SetActive(false); }
 
-		public void ShowComment()
-		{
-			_commentRoot.SetActive(true);
+			await FadeInPanel(_victoryRoot, 0.5f);
 		}
-		public void HideComment()
+		public async UniTask HideVictory(float duration = 0.3f)
 		{
-			_commentRoot.SetActive(false);
+			await FadeOutPanel(_victoryRoot, duration);
 		}
 
-		public void ShowRetry()
+		public async UniTask ShowComment()
 		{
-			_retryRoot.SetActive(true);
+			await FadeInPanel(_commentRoot, 0.5f);
 		}
-		public void HideRetry()
+		public async UniTask HideComment(float duration = 0.3f)
 		{
-			_retryRoot.SetActive(false);
-		}
-
-		public void SetCounter(int deathCounter)
-		{
-			_countdownImages[0].gameObject.SetActive(deathCounter < 1);
-			_countdownImages[1].gameObject.SetActive(deathCounter < 2);
-			_countdownImages[2].gameObject.SetActive(deathCounter < 3);
+			await FadeOutPanel(_commentRoot, duration);
 		}
 
-		public void ShowGiveUp(int deathCounter)
+		public async UniTask ShowRetry()
+		{
+			await FadeInPanel(_retryRoot, 0.5f);
+		}
+		public async UniTask HideRetry(float duration = 0.3f)
+		{
+			await FadeOutPanel(_retryRoot, duration);
+		}
+
+		public async UniTask ShowGiveUp(int deathCounter)
 		{
 			_giveUpText.text = deathCounter switch
 			{
@@ -100,24 +116,58 @@ namespace GameJam
 				1 => $"Again? \n\nWhy don't you just give up already?!",
 				_ => $"You can't do this alone... \n\nGive up?"
 			};
-			_giveUpRoot.SetActive(true);
+			await FadeInPanel(_giveUpRoot, 0.5f);
 		}
-		public void HideGiveUp() { _giveUpRoot.SetActive(false); }
+		public async UniTask HideGiveUp(float duration = 0.3f)
+		{
+			await FadeOutPanel(_giveUpRoot, duration);
+		}
 
-		public void ShowReceiveHelp(string helperName)
+		public async UniTask ShowReceiveHelp(string helperName)
 		{
 			_receiveHelpText.text = $"Help offer received from \"{helperName}\".\n\nAccept the offer?";
-			_receiveHelpRoot.SetActive(true);
-
+			await FadeInPanel(_receiveHelpRoot, 0.5f);
 		}
-		public void HideReceiveHelp() { _receiveHelpRoot.SetActive(false); }
-
-		public async UniTask FadeIn(Color color, float duration = 1f) { await _fadeToBlackImage.DOColor(color, duration); }
-		public async UniTask FadeOut(float duration = 1f) { await _fadeToBlackImage.DOColor(Color.clear, duration); }
-
-		public void SetDebugText(string value)
+		public async UniTask HideReceiveHelp(float duration = 0.3f)
 		{
-			_debugText.text = value;
+			await FadeOutPanel(_receiveHelpRoot, duration);
+		}
+
+		public async UniTask FadeIn(Color color, float duration = 1f)
+		{
+			await _fadeToBlackImage.DOColor(color, duration);
+		}
+		public async UniTask FadeOut(float duration = 1f)
+		{
+			await _fadeToBlackImage.DOColor(Color.clear, duration);
+		}
+
+		private async UniTask FadeInPanel(Image panel, float duration)
+		{
+			panel.gameObject.SetActive(true);
+			panel.DOFade(1f, duration);
+
+			var graphics = panel.GetComponentsInChildren<Graphic>();
+			foreach (var graphic in graphics)
+			{
+				graphic.DOFade(1f, duration);
+			}
+
+			await UniTask.Delay(TimeSpan.FromSeconds(duration));
+		}
+
+		private async UniTask FadeOutPanel(Image panel, float duration)
+		{
+			panel.DOFade(0f, duration);
+
+			var graphics = panel.GetComponentsInChildren<Graphic>();
+			foreach (var graphic in graphics)
+			{
+				graphic.DOFade(0f, duration);
+			}
+
+			await UniTask.Delay(TimeSpan.FromSeconds(duration));
+			panel.gameObject.SetActive(false);
 		}
 	}
 }
