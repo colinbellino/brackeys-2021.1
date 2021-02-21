b486738a83626aed414d6b5850ec025126ce8795
Colin Bellino <bellino.colin@gmail.com>
2021-02-21 10:39:44 +0100

    🐛 Fix camera bounds

--- Assets/Code/GameJam/Game.cs
+++ Assets/Code/GameJam/Game.cs
@@ -19,7 +19,7 @@ namespace GameJam
 		public const int HELPERS_MAX_COUNT = 5;
 
 		public static Bounds Bounds = new Bounds(Vector3.zero, new Vector3(45f, 28f, 1f));
-		public static Bounds MoveBounds = new Bounds(Vector3.zero, new Vector3(41f, 20f, 1f));
+		public static Bounds MoveBounds = new Bounds(Vector3.zero, new Vector3(41f, 22f, 1f));
 		public static readonly List<string> PlaceholderNames = new List<string>(new []{
 			"Micah",
 			"Vernon",
